6a4c6b09f0a563e33503bf41d4cdfd86230594b5
Dezang <dezang@dezang.net>
2021-02-28 12:11:07 +0900

    chore: 최초 커밋

---
+++ README.md
@@ -0,0 +1,4 @@
+# Test-Driven Development
+
+켄트백이 쓴 `Test-Driven Development` 를 공부하면서 예제를 작접 타이핑하는 프로젝트
+
---
+++ tdd/src/main/java/net/dezang/TddApplication.java
@@ -0,0 +1,7 @@
+package net.dezang;
+
+public class TddApplication {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
